e5aacd66132038159076fe186b48a92559ddbf0a
hakan efe <hakanforwork@gmail.com>
2023-03-12 19:30:33 +0200

    week 4

---
+++ Week4/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Random rnd = new Random();
+
+int myRandom;
+
+for (int i = 0; i< 7; i++)
+{
+ myRandom = rnd.Next(1, 11);
+Console.WriteLine($" {i +1}. {myRandom}");
+}
+
+
+
+
+
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"{i+ 1}. ı will not skip zoom classes");
+//}
+
